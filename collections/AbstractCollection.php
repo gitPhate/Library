4bd1a5e4866a8e32209d9a63a9594e6fd3129935
Phate <gabry.phate@gmail.com>
2015-05-05 19:54:27 +0200

    fixed interface mismatch in comments

--- collections/AbstractCollection.php
+++ collections/AbstractCollection.php
@@ -25,7 +25,7 @@ abstract class AbstractCollection implements IBaseCollection, \ArrayAccess
         }
     }
     
-    //ICollection implementation
+    //IBaseCollection implementation
     
     public function Any()
     {
