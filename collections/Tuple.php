f8d63f8773700bb6e5d6aa5a20c46d8dcf085032
Phate <gabry.phate@gmail.com>
2015-05-05 19:56:44 +0200

    added Tuple class
    Added Tuple class, similar to the one in the .net framework. One can e.g. create multidimentional dictionaries using this class.
    Fixes #11

---
+++ collections/Tuple.php
@@ -0,0 +1,38 @@
+<?php
+namespace Library\Collections;
+
+use Library\Exceptions\ArgumentException;
+
+class Tuple
+{
+	public $Item1;
+	public $Item2;
+	public $Item3;
+	public $Item4;
+	public $Item5;
+	public $Item6;
+	public $Item7;
+	public $Rest;
+	
+	public function __construct()
+	{
+		$args = func_get_args();
+		
+		if(count($args) > 8)
+		{
+			throw new ArgumentException("Elements in a tuple are at most 8");
+		}
+		
+		if(count($args) == 0)
+		{
+			throw new ArgumentException("There must be at least one element in the tuple");
+		}
+		
+		for($i = 1;$i <= count($args); $i++)
+		{
+			$fieldName = "Item".$i;
+			$this->$fieldName = $args[$i - 1];
+		}
+	}
+}
+?>
