39e6d252ed639ac175f7ec5c9a2131a5a6cd550e
gitPhate <gabry.phate@gmail.com>
2015-05-13 16:52:48 +0200

    Improved autoload

--- autoload.php
+++ autoload.php
@@ -1,13 +1,10 @@
 <?php
-function autoload($name)
-{
+spl_autoload_register(function ($name) {
     //echo "loading {$name}.php<br />";
     
     if(preg_match("/^library\\\\/i", $name))
     {
         require_once($name.".php");
     }
-}
-
-spl_autoload_register("autoload");
+});
 ?>
